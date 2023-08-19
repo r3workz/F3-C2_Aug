a5be9490b75dea6ac6ff2d168ad6b4218242872c
r3WorkZ-wsl <122603849+r3workz@users.noreply.github.com>
2023-08-20 02:04:44 +0530

    added PO list

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mainScreen = document.getElementById("mainPage")
 
 const startBtn = document.getElementById("btnStart")
 
+const ipHomeField = document.getElementById("ip")
 const ipAddressField = document.getElementById("userIp")
 const latitudeField = document.getElementById("userLat")
 const longitudeField = document.getElementById("userLong")
@@ -18,10 +19,29 @@ const messageField = document.getElementById("userMsg")
 
 const mapBox = document.getElementById("mapBox")
 
-async function fillUserData() {
+const searchInput = document.getElementById("searchPO")
 
+const  poListContainer = document.getElementById("viewPostOffices")
+function json(url) {
+    return fetch(url).then(res => res.json());
+}
+
+async function fetchData() {
+    try{
+        const request = await fetch("https://ipinfo.io/json?token=4e42a56b19eaad")
+        return await request.json();
+
+    }catch (error){
+        console.log(error)
+        document.body.alert(error);
+    }
+}
+
+
+
+async function fillUserData() {
     // // This was redundant
-    // const apiKey = 'NVM';
+    // const apiKey = 'API_KEY';
     // function json(url) {
     //     return fetch(url).then(res => res.json());
     // }
@@ -29,52 +49,113 @@ async function fillUserData() {
     //     .then(data => {
     //    set dom data here
     //     });
+    fetchData().then((userData)=>{
+        ipAddressField.textContent = `${userData.ip}`;
+        cityField.textContent = `${userData.city}`;
+        regionField.textContent = `${userData.region}, ${userData.country}`;
+        latitudeField.textContent = `${userData.loc.split(',')[0]}`;
+        longitudeField.textContent = `${userData.loc.split(',')[1]}`;
+        timeZoneField.textContent = `${userData.timezone}`
+        organisationField.textContent = `${userData.org}`
 
-    const request = await fetch("https://ipinfo.io/json?token=4e42a56b19eaad")
-    const data = await request.json()
+        let formattedTime = new Date().toLocaleString("en-US",
+            {
+                timeZone: userData.timezone,
+                // format: "dd/MM/yyyy, HH:mm:ss"
+            });
+        dateTimeField.textContent = `${formattedTime}`
+        pinCodeField.textContent = `${userData.postal}`
+        getPO(userData.postal)
 
-    console.log(data)
+        messageField.textContent = `Hello Welcome to my Contest Project`
+        mapBox.innerHTML = `<iframe src="https://maps.google.com/maps?q=${userData.loc}&z=13&output=embed"></iframe>`
 
-    ipAddressField.textContent = `${data.ip}`;
-    cityField.textContent = `${data.city}`;
-    regionField.textContent = `${data.region}, ${data.country}`;
-    latitudeField.textContent = `${data.loc.split(',')[0]}`;
-    longitudeField.textContent = `${data.loc.split(',')[1]}`;
-    timeZoneField.textContent = `${data.timezone}`
-    organisationField.textContent = `${data.org}`
-
-    let formattedTime = new Date().toLocaleString("en-US",
-        {
-            timeZone: data.timezone,
-            // format: "dd/MM/yyyy, HH:mm:ss"
-        });
-    dateTimeField.textContent = `${formattedTime}`
-    pinCodeField.textContent = `${data.postal}`
-
-    messageField.textContent = `Hello Welcome to my Contest Project`
-    mapBox.innerHTML = `<iframe src="https://maps.google.com/maps?q=${data.loc}&z=13&output=embed"></iframe>`
-
-
+    }).catch((error)=>{
+        console.log(error)
+        document.body.alert(error);
+    });
 
 
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
+    fetchData().then((data)=>{
+        ipHomeField.innerText=data.ip;
+    }).catch((error)=>{
+        console.log(error)
+        document.body.alert(error);
+    });
     startScreen.classList.remove('hidden');
     loaderScreen.classList.add('hidden')
 })
 
-startBtn.addEventListener("click",()=>{
+startBtn.addEventListener("click",async () => {
     startScreen.classList.add('hidden');
     loaderScreen.classList.remove('hidden')
-    fillUserData();
-    setTimeout(()=> {
+    await fillUserData();
+    setTimeout(() => {
             mainScreen.classList.remove('hidden')
             loaderScreen.classList.add('hidden')
-        }
-        ,4000);
-
-
+        },
+        // 0
+        2000
+    );
 })
 
-// TODO fetch & list PostOffices
+
+function getPO(pinCode) {
+    json(`https://api.postalpincode.in/pincode/${pinCode}`)
+        .then(data => {
+            for (const postOffice of data[0].PostOffice) {
+                const div = document.createElement("div");
+                div.classList.add("poListItem");
+
+                const nameDiv = document.createElement("div");
+                const nameSpan = document.createElement("span");
+                nameSpan.id = "poName";
+                nameSpan.innerText = postOffice.Name;
+                nameDiv.innerHTML = `Name:`;
+                nameDiv.appendChild(nameSpan)
+                div.appendChild(nameDiv);
+
+                const branchTypeDiv = document.createElement("div");
+                branchTypeDiv.innerText = "Branch Type: ";
+                const branchTypeSpan = document.createElement("span");
+                branchTypeSpan.id = "poBranchType";
+                branchTypeSpan.innerText = postOffice.BranchType;
+                branchTypeDiv.appendChild(branchTypeSpan)
+                div.appendChild(branchTypeDiv);
+
+                const delStatsDiv = document.createElement("div");
+                delStatsDiv.innerText = "Delivery Status: ";
+                const delStatsSpan = document.createElement("span");
+                delStatsSpan.id = "poDelStats";
+                delStatsSpan.innerText = postOffice.DeliveryStatus;
+                delStatsDiv.appendChild(delStatsSpan);
+                div.appendChild(delStatsDiv);
+
+                const distDiv = document.createElement("div");
+                distDiv.innerText = "District: ";
+                const distSpan = document.createElement("span");
+                distSpan.id = "poDist";
+                distSpan.innerText = postOffice.District;
+                distDiv.appendChild(distSpan);
+                div.appendChild(distDiv);
+
+                const divDivision = document.createElement("div");
+                divDivision.innerText = "Division: ";
+                const divSpan = document.createElement("span");
+                divSpan.id = "poDivision";
+                divSpan.innerText = postOffice.Division;
+                divDivision.appendChild(divSpan);
+                div.appendChild(divDivision);
+
+                poListContainer.appendChild(div)
+            }
+        }).catch((error)=>{
+            console.log(error)
+            document.body.alert(error);
+    });
+}
+
+// TODO - SearchFunction
